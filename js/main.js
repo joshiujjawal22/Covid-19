46f31835a0ca4d7fdd8b575543032c1311c0e949
UJJAWAL JOSHI <joshiujjawal22@gmail.com>
2020-03-28 22:49:56 +0530

    Add files via upload

--- js/main.js
+++ js/main.js
@@ -3,8 +3,8 @@ function get(){
 var a=document.getElementById("mySelect");
 
 
-// 27 March of 2020
-if(a.options[a.selectedIndex].text=="27 MAR 2020"){
+// 26 March of 2020
+if(a.options[a.selectedIndex].text=="26 MAR 2020"){
 	// INDIA
 	document.getElementById("i_1").innerHTML="727";
 	document.getElementById("i_2").innerHTML="20";
@@ -76,8 +76,8 @@ if(a.options[a.selectedIndex].text=="27 MAR 2020"){
 
 }
 
-// 28 March of 2020
-else if(a.options[a.selectedIndex].text=="28 MAR 2020"){
+// 27 March of 2020
+else if(a.options[a.selectedIndex].text=="27 MAR 2020"){
 	// INDIA
 	document.getElementById("i_1").innerHTML="753";
 	document.getElementById("i_2").innerHTML="20";
@@ -131,8 +131,8 @@ else if(a.options[a.selectedIndex].text=="28 MAR 2020"){
 	document.getElementById("sw_2").innerHTML="197";
 	document.getElementById("sw_3").innerHTML="897";
 	// UK
-	document.getElementById("UK_1").innerHTML="11658";
-	document.getElementById("UK_2").innerHTML="578";
+	document.getElementById("UK_1").innerHTML="14543";
+	document.getElementById("UK_2").innerHTML="759";
 	document.getElementById("UK_3").innerHTML="135";
 	// CANADA
 	document.getElementById("ca_1").innerHTML="4043";
@@ -148,75 +148,75 @@ else if(a.options[a.selectedIndex].text=="28 MAR 2020"){
 	document.getElementById("iraq_3").innerHTML="122";
 }
 
-// 29 March of 2020
-else if(a.options[a.selectedIndex].text=="29 MAR 2020"){
+// 28 March of 2020
+else if(a.options[a.selectedIndex].text=="28 MAR 2020"){
 	// INDIA
-	document.getElementById("i_1").innerHTML="753";
+	document.getElementById("i_1").innerHTML="933";
 	document.getElementById("i_2").innerHTML="20";
-	document.getElementById("i_3").innerHTML="67";
+	document.getElementById("i_3").innerHTML="84";
 	// CHINA
-	document.getElementById("c_1").innerHTML="81340";
-	document.getElementById("c_2").innerHTML="3292";
-	document.getElementById("c_3").innerHTML="74588";
+	document.getElementById("c_1").innerHTML="81394";
+	document.getElementById("c_2").innerHTML="3295";
+	document.getElementById("c_3").innerHTML="74971";
 	// USA
-	document.getElementById("U_1").innerHTML="85749";
-	document.getElementById("U_2").innerHTML="1304";
-	document.getElementById("U_3").innerHTML="1868";
+	document.getElementById("U_1").innerHTML="112560";
+	document.getElementById("U_2").innerHTML="1878";
+	document.getElementById("U_3").innerHTML="3219";
 	// ITALY
-	document.getElementById("it_1").innerHTML="80589";
-	document.getElementById("it_2").innerHTML="8215";
-	document.getElementById("it_3").innerHTML="10361";
+	document.getElementById("it_1").innerHTML="86498";
+	document.getElementById("it_2").innerHTML="9134";
+	document.getElementById("it_3").innerHTML="10950";
 	// SPAIN
-	document.getElementById("sp_1").innerHTML="64059";
-	document.getElementById("sp_2").innerHTML="4858";
-	document.getElementById("sp_3").innerHTML="9357";
+	document.getElementById("sp_1").innerHTML="72248";
+	document.getElementById("sp_2").innerHTML="5812";
+	document.getElementById("sp_3").innerHTML="12285";
 	// GERMANY
-	document.getElementById("ge_1").innerHTML="47278";
-	document.getElementById("ge_2").innerHTML="281";
-	document.getElementById("ge_3").innerHTML="5673";
+	document.getElementById("ge_1").innerHTML="53340";
+	document.getElementById("ge_2").innerHTML="399";
+	document.getElementById("ge_3").innerHTML="6658";
 	// IRAN
-	document.getElementById("ir_1").innerHTML="32332";
-	document.getElementById("ir_2").innerHTML="2378";
-	document.getElementById("ir_3").innerHTML="11133";
+	document.getElementById("ir_1").innerHTML="35408";
+	document.getElementById("ir_2").innerHTML="2517";
+	document.getElementById("ir_3").innerHTML="11679";
 	// FRANCE
-	document.getElementById("fr_1").innerHTML="29155";
-	document.getElementById("fr_2").innerHTML="1696";
-	document.getElementById("fr_3").innerHTML="4948";
+	document.getElementById("fr_1").innerHTML="32964";
+	document.getElementById("fr_2").innerHTML="1995";
+	document.getElementById("fr_3").innerHTML="5700";
 	// SOUTH KOREA
-	document.getElementById("sk_1").innerHTML="9332";
-	document.getElementById("sk_2").innerHTML="139";
-	document.getElementById("sk_3").innerHTML="4528";
+	document.getElementById("sk_1").innerHTML="9478";
+	document.getElementById("sk_2").innerHTML="144";
+	document.getElementById("sk_3").innerHTML="4811";
 	// NETHERLAND
-	document.getElementById("ne_1").innerHTML="7431";
-	document.getElementById("ne_2").innerHTML="434";
+	document.getElementById("ne_1").innerHTML="9762";
+	document.getElementById("ne_2").innerHTML="639";
 	document.getElementById("ne_3").innerHTML="3";
 	// AUSTRALIA
-	document.getElementById("australia_1").innerHTML="3180";
-	document.getElementById("australia_2").innerHTML="13";
+	document.getElementById("australia_1").innerHTML="3635";
+	document.getElementById("australia_2").innerHTML="14";
 	document.getElementById("australia_3").innerHTML="170";
 	// AUSTRIA
-	document.getElementById("austria_1").innerHTML="7317";
-	document.getElementById("austria_2").innerHTML="58";
+	document.getElementById("austria_1").innerHTML="8030";
+	document.getElementById("austria_2").innerHTML="68";
 	document.getElementById("austria_3").innerHTML="225";
 	// SWITZERLAND
-	document.getElementById("sw_1").innerHTML="11951";
-	document.getElementById("sw_2").innerHTML="197";
-	document.getElementById("sw_3").innerHTML="897";
+	document.getElementById("sw_1").innerHTML="13377";
+	document.getElementById("sw_2").innerHTML="242";
+	document.getElementById("sw_3").innerHTML="1530";
 	// UK
-	document.getElementById("UK_1").innerHTML="11658";
-	document.getElementById("UK_2").innerHTML="578";
+	document.getElementById("UK_1").innerHTML="17089";
+	document.getElementById("UK_2").innerHTML="1019";
 	document.getElementById("UK_3").innerHTML="135";
 	// CANADA
 	document.getElementById("ca_1").innerHTML="4043";
 	document.getElementById("ca_2").innerHTML="39";
 	document.getElementById("ca_3").innerHTML="228";
 	// JAPAN
-	document.getElementById("ja_1").innerHTML="1387";
-	document.getElementById("ja_2").innerHTML="47";
-	document.getElementById("ja_3").innerHTML="359";
+	document.getElementById("ja_1").innerHTML="1499";
+	document.getElementById("ja_2").innerHTML="49";
+	document.getElementById("ja_3").innerHTML="404";
 	// IRAQ
-	document.getElementById("iraq_1").innerHTML="458";
-	document.getElementById("iraq_2").innerHTML="40";
-	document.getElementById("iraq_3").innerHTML="122";
+	document.getElementById("iraq_1").innerHTML="506";
+	document.getElementById("iraq_2").innerHTML="42";
+	document.getElementById("iraq_3").innerHTML="131";
 }
 }
